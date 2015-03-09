f45b44072dadd781ae9da41c92cf24f46b989613
Hamidreza Jahtalab <hamid@tosanidm.net>
2015-03-09 16:23:18 +0330

    add TestWebDavProcessor

---
+++ NDav.Tests/TestWebDavProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDav.Tests
+{
+    public class TestWebDavProcessor
+    {
+        public void Test()
+        {
+            
+           // var webdav = new WebDavProcessor();
+            var request = HttpWebRequest.CreateHttp("http://localhost/test");
+            request.Method = "GET";
+        }
+    }
+}
